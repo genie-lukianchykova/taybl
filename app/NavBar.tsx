f622bfb09575a2878bf40ab48a2a50787c23b67a
Yevheniia <genie.lukianchykova@gmail.com>
2025-09-29 20:49:34 +0200

    Create NavBar

---
+++ app/NavBar.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation';
+import React from 'react'
+import { MdCoffeeMaker } from "react-icons/md";
+import classnames from 'classnames';
+
+const NavBar = () => {
+    const currentPath = usePathname();
+
+    const links = [
+        { href: "/", label: "Dashboard" },
+        { href: "/orders", label: "Orders" },
+    ]
+  
+    return (
+        <nav className='flex space-x-6 border-b border-zinc-300 mb-5 px-5 h-14 items-center'>
+            <Link href="/"><MdCoffeeMaker className='text-2xl' /></Link>
+            <ul className='flex space-x-6'>
+                {links.map(link => 
+                    <Link 
+                        key={link.href} 
+                        className={classnames({
+                            'text-zinc-900': link.href === currentPath,
+                            'text-zinc-500': link.href !== currentPath,
+                            'hover:text-zinc-800 transition-colors': true
+                        })}
+                        href={link.href}>
+                            {link.label}
+                    </Link>
+                )}
+            </ul>
+    </nav>
+  )
+}
+
+export default NavBar
